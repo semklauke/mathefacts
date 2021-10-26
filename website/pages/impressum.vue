c8e4d7e4cd7fe19a97068a942efbcd0347adbe0c
semklauke <privat@semklauke.de>
2021-10-26 19:58:00 +0200

    added impressum page

---
+++ website/pages/impressum.vue
@@ -0,0 +1,31 @@
+<template>
+<b-container fluid="lg" class="facts_group">
+    <h1>Impressum</h1>
+
+    <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+    <p>Sem Klauke<br />
+    Vaalser Stra&szlig;e, 432<br />
+    52074 Aachen</p>
+
+    <h2>Kontakt</h2>
+    <p>Telefon: &#91;Telefonnummer&#93;<br />
+    E-Mail: mathefacts@semklauke.de</p>
+
+    <h2>Redaktionell verantwortlich</h2>
+    <p>Sem Klauke</p>
+
+    <!--p>Quelle: <a href="https://www.e-recht24.de">https://www.e-recht24.de</a></p-->
+</b-container>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+})
+</script>
+
+<style>
+
+</style>
